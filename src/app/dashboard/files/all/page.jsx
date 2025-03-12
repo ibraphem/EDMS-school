49fcb3e31a5dfbf86110d9a439e093061a1bdd9e
ghaalib <olaolajide99@gmail.com>
2025-03-12 14:26:58 +0100

    commit

---
+++ src/app/dashboard/files/all/page.jsx
@@ -0,0 +1,12 @@
+import AllFiles from "@/screens/dashboard/AllFiles";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <AllFiles />
+    </>
+  );
+};
+
+export default page;
